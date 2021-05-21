a8ca8f7291495b23c874b6f2b17a9c3ee4fb035d
pkmstdj <pkmstdj@gmail.com>
2021-05-21 18:33:51 +0900

    0521/in work user fragment

--- app/src/main/java/dcp/n/boardtest02/ui/user/UserFragment.java
+++ app/src/main/java/dcp/n/boardtest02/ui/user/UserFragment.java
@@ -9,45 +9,29 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 import dcp.n.boardtest02.R;
+import dcp.n.boardtest02.ReadActivity;
 
 public class UserFragment extends Fragment {
     public static final String TAG = "logT";
-    private static final int RC_SIGN_IN = 9001;
 
     private FirebaseAuth mAuth = null;
-    private GoogleSignInClient mGoogleSignInClient;
-    private SignInButton signInButton;
+    private RecyclerView recyclerView;
+    private UserAdapter adapter;
+    private List<UserItemModel> list;
 
     private UserViewModel mViewModel;
 
@@ -60,27 +44,22 @@ public class UserFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         View root;
         mAuth = FirebaseAuth.getInstance();
-//        if(mAuth.getCurrentUser() != null) {
-            root = inflater.inflate(R.layout.fragment_user, container, false);
-            FirebaseUser user = mAuth.getCurrentUser();
-            setUser(root, user);
-//        }
-//        else {
-//            root = inflater.inflate(R.layout.fragment_login, container, false);
-//            GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-//                    .requestIdToken(getString(R.string.default_web_client_id))
-//                    .requestEmail()
-//                    .build();
-//            mGoogleSignInClient = GoogleSignIn.getClient(getActivity(), gso);
-//
-//            root.findViewById(R.id.signInButton).setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-//                    startActivityForResult(signInIntent, RC_SIGN_IN);
-//                }
-//            });
-//        }
+        root = inflater.inflate(R.layout.fragment_user, container, false);
+        FirebaseUser user = mAuth.getCurrentUser();
+        setUser(root, user);
+
+
+        recyclerView = root.findViewById(R.id.recyclerview_board);
+        list = new ArrayList<>();
+
+        list.add(new UserItemModel("id", "사역 신청이 왔습니다", "테스트", "admin"));
+        list.add(new UserItemModel("id", "답변이 왔습니다.", "테스트", "admin"));
+        list.add(new UserItemModel("id", "가입을 환영합니다", "테스트", "admin"));
+        list.add(new UserItemModel("id", "쓸게 없습니다", "테스트", "admin"));
+        list.add(new UserItemModel("id", "하나더", "테스트", "admin"));
+
+        adapter = new UserAdapter(list);
+        recyclerView.setAdapter(adapter);
 
         return root;
     }
@@ -91,93 +70,69 @@ public class UserFragment extends Fragment {
         mViewModel = new ViewModelProvider(this).get(UserViewModel.class);
         // TODO: Use the ViewModel
     }
-    /*
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            try {
-                // Google Sign In was successful, authenticate with Firebase
-                GoogleSignInAccount account = task.getResult(ApiException.class);
-                Log.d(TAG, "firebaseAuthWithGoogle:" + account.getId());
-                firebaseAuthWithGoogle(account.getIdToken());
-            } catch (ApiException e) {
-                // Google Sign In failed, update UI appropriately
-                Log.w(TAG, "Google sign in failed", e);
-            }
-        }
-    }
-    private void firebaseAuthWithGoogle(String idToken) {
-        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
-        mAuth.signInWithCredential(credential)
-                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            FirebaseFirestore firestore = FirebaseFirestore.getInstance();
-                            CollectionReference collection = firestore.collection("users").document(user.getUid()).collection("data");
 
-                            Map<String, Object> post = new HashMap<>();
-                            post.put("uid", user.getUid());
-                            post.put("name", user.getDisplayName());
-                            post.put("email", user.getEmail());
-                            collection.document("account").set(post).addOnSuccessListener(new OnSuccessListener<Void>() {
-                                @Override
-                                public void onSuccess(Void aVoid) {
-                                    Log.d(TAG, "DocumentSnapshot successfully written!");
-                                }
-                            })
-                            .addOnFailureListener(new OnFailureListener() {
-                                @Override
-                                public void onFailure(@NonNull Exception e) {
-                                    Log.w(TAG, "Error writing document", e);
-                                }
-                            });
-//                            collection.add(post).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-//                                @Override
-//                                public void onSuccess(DocumentReference documentReference) {
-//                                    Log.d(TAG, "FirebaseFirestore collection:success");
-//                                }
-//                            }).addOnFailureListener(new OnFailureListener() {
-//                                @Override
-//                                public void onFailure(@NonNull Exception e) {
-//                                    Log.d(TAG, "FirebaseFirestore collection:success");
-//                                }
-//                            });
-                            updateUI(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "signInWithCredential:failure", task.getException());
-                            updateUI(null);
-                        }
-                    }
-                });
-    }
-
-     */
-    private void updateUI(FirebaseUser user) { //update ui code here
-        if (user != null) {
-            LayoutInflater inflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            View root = inflater.inflate(R.layout.fragment_user, null);
-            ViewGroup rootView = (ViewGroup) getView();
-            rootView.removeAllViews();
-            rootView.addView(root);
-            setUser(root, user);
-
-//            Intent intent = new Intent(this, AfterActivity.class);
-//            startActivity(intent);
-//            finish();
-        }
-    }
     private void setUser(View root, FirebaseUser user) {
         TextView tvName = root.findViewById(R.id.tv_user_name);
         TextView tvMail = root.findViewById(R.id.tv_user_mail);
 
         tvName.setText(user.getDisplayName());
         tvMail.setText(user.getEmail());
+    }
+    private class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder> {
+
+        private List<UserItemModel> modelList;
+
+        public UserAdapter(List<UserItemModel> modelList) {
+            this.modelList = modelList;
+        }
+
+        @NonNull
+        @Override
+        public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new UserViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.seek_board_item, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
+            UserItemModel data = modelList.get(position);
+            holder.txtTitle.setText(data.getTitle());
+            holder.txtContents.setText(data.getContents());
+            holder.txtName.setText(data.getName());
+        }
+
+        @Override
+        public int getItemCount() {
+            return modelList.size();
+        }
+
+        class UserViewHolder extends RecyclerView.ViewHolder {
+
+            private TextView txtTitle;
+            private TextView txtContents;
+            private TextView txtName;
+
+            public UserViewHolder(@NonNull View itemView) {
+                super(itemView);
+
+                txtTitle = itemView.findViewById(R.id.item_board_title);
+                txtContents = itemView.findViewById(R.id.item_board_contents);
+                txtName = itemView.findViewById(R.id.item_board_name);
+
+                itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String title = txtTitle.getText().toString();
+                        String contents = txtContents.getText().toString();
+                        String name = txtName.getText().toString();
+
+                        Intent intent = new Intent(getActivity(), ReadActivity.class);
+                        intent.putExtra("title", title);
+                        intent.putExtra("contents", contents);
+                        intent.putExtra("name", name);
+                        startActivity(intent);
+                    }
+                });
+            }
+        }
     }
 }
